32b75dbde8146cfce0153336526157bf44d8a9f1
Pavlo Tereshko <pavlo.tereshko@gmail.com>
2024-02-08 22:06:33 +0200

    Minor fixes

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,12 @@
-import { Profile } from "./components/Profile/Profile";
-import { Statistics } from "./components/Statistics/Statistics";
-import { FriendList } from "./components/FriendList/FriendList";
-import { TransactionHistory } from "./components/TransactionHistory/TransactionHistory";
+import { Profile } from './components/Profile/Profile';
+import { Statistics } from './components/Statistics/Statistics';
+import { FriendList } from './components/FriendList/FriendList';
+import { TransactionHistory } from './components/TransactionHistory/TransactionHistory';
 
-
-import user from "./data/user";
-import data from "./data/data";
-import friends from "./data/friends";
-import transactions from "./data/transactions";
+import user from './data/user';
+import data from './data/data';
+import friends from './data/friends';
+import transactions from './data/transactions';
 
 export const App = () => {
   return (
